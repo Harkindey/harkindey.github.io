626674b371fcc557a795feb98fbf2385f17c2e38
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2020-02-02 10:01:44 +0100

    PDF

--- Readme.md
+++ Readme.md
@@ -1,2 +1,7 @@
 My Resume.
 [EmmanuelAkindeCV](https://harkindey.github.io/)
+
+Tools
+
+-   https://html2pdf.com/
+-   https://foundation.zurb.com/emails/inliner-v2.html
--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-const puppeteer = require('puppeteer');
+// const puppeteer = require('puppeteer');
+const wkhtmltopdf = require('wkhtmltopdf');
+var fs = require('fs');
 
-(async () => {
-	const browser = await puppeteer.launch({ headless: true });
-	const page = await browser.newPage();
-	await page.goto('https://harkindey.github.io/', {
-		waitUntil: 'networkidle2',
-	});
-	await page.pdf({ path: 'EmmanuelAkindeCV.pdf', format: 'A4' });
+// (async () => {
+// 	const browser = await puppeteer.launch({ headless: true });
+// 	const page = await browser.newPage();
+// 	await page.goto('https://harkindey.github.io/', {
+// 		waitUntil: 'networkidle2',
+// 	});
+// 	await page.emulateMedia('screen');
+// 	await page.pdf({
+// 		path: 'EmmanuelAkindeCV.pdf',
+// 		format: 'A4',
+// 		// preferCSSPageSize: true,
+// 		// printBackground: true,
+// 	});
 
-	await browser.close();
-})();
+// 	await browser.close();
+// })();
+
+wkhtmltopdf('https://harkindey.github.io/', { pageSize: 'letter' }).pipe(
+	fs.createWriteStream('out.pdf')
+);
